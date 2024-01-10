2d2ac94ecd36c7cc8b8a6b1f4dfecc03aa91a217
yamadaOliva <vietanh87.forwork@gmail.com>
2024-01-11 02:56:58 +0700

    update login

--- src/page/register/register.jsx
+++ src/page/register/register.jsx
@@ -1,6 +1,9 @@
 import "./register.scss";
 import { getMajor } from "../../service/helper.service";
 import { useEffect, useState } from "react";
+import { useTheme } from "@mui/material";
+import { toast } from "react-toastify";
+import axios from "axios";
 import {
   Avatar,
   Grid,
@@ -10,26 +13,123 @@ import {
   Button,
   FormControlLabel,
   Checkbox,
+  Box,
+  OutlinedInput,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Chip,
 } from "@material-ui/core";
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Du lịch",
+  "Đọc sách",
+  "Nghe nhạc",
+  "Chơi thể thao",
+  "Nấu ăn",
+  "Học ngoại ngữ",
+  "Xem phim",
+  "Viết blog",
+  "Chơi game",
+  "Học hỏi mới",
+  "Yoga",
+  "Lập trình",
+  "Thiền",
+  "Nghệ thuật sáng tạo",
+  "Chụp ảnh",
+  "Gardening",
+  "Thể dục buổi sáng",
+  "Meditation",
+  "Nấu ăn",
+  "Ghi chép nhật ký",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 export default function Register() {
   //useState
+  const theme = useTheme();
+  const [personName, setPersonName] = useState([]);
   const [major, setMajor] = useState([]);
-  const [currentMajor, setCurrentMajor] = useState([]); // [1,2,3
+  const [currentMajor, setCurrentMajor] = useState([]);
+  const [classCount, setClassCount] = useState(1);
+  const [classNumber, setClassNumber] = useState(1);
+  const [province, setProvince] = useState([]);
+  const [currentProvince, setCurrentProvince] = useState({});
+  const [district, setDistrict] = useState([]);
+  const [currentDistrict, setCurrentDistrict] = useState("");
   //useEffect
   useEffect(() => {
     const get = async () => {
-      const res = await getMajor();
-      setMajor(res.data);
-      setCurrentMajor(res.data[0]);
-      console.log(res);
+      try {
+        const res = await getMajor();
+        setMajor(res.data);
+        setCurrentMajor(res.data[1]);
+        setClassCount(res.data[1].classCount[0].schoolYear);
+        setClassNumber(res.data[1].classCount[0].count);
+        let ptr = await axios.get("https://provinces.open-api.vn/api/");
+        let ptr1 = await axios.get(
+          `https://provinces.open-api.vn/api/p/${ptr.data[0].code}?depth=2`
+        );
+        console.log(ptr1.data.districts);
+        setProvince(ptr.data);
+        setCurrentProvince(ptr.data[0]);
+        setDistrict(ptr1.data.districts);
+        console.log(ptr1.data.districts[0]);
+        setCurrentDistrict(ptr1.data.districts[0].name);
+      } catch (error) {
+        console.log(error);
+      }
     };
     get();
   }, []);
+  const getDistrict = async (id) => {
+    try {
+      let ptr = await axios.get(
+        `https://provinces.open-api.vn/api/p/${id}?depth=2`
+      );
+      setDistrict(ptr.data.districts);
+      console.log(ptr.data.districts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   //style
   const paperStyle = {
     padding: 20,
     height: "auto",
     width: 800,
+  };
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    //check max 5
+    if (value.length > 5) {
+      toast.error("Bạn chỉ được chọn tối đa 5 sở thích");
+      return;
+    }
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
   };
   return (
     <>
@@ -45,7 +145,7 @@ export default function Register() {
             </Grid>
             <form>
               <Grid container spacing={2}>
-                <Grid xs={12} sm={6} className="pr-4">
+                <Grid xs={6} className="pr-4">
                   <Grid container spacing={2}>
                     {/* <Grid item xs={12} sm={6}>
                     <TextField
@@ -55,7 +155,7 @@ export default function Register() {
                       required
                     />
                   </Grid> */}
-                  <Grid item xs={12}>
+                    <Grid item xs={12}>
                       <TextField
                         label="Mã sinh viên"
                         placeholder="Nhập mã sinh viên"
@@ -71,6 +171,56 @@ export default function Register() {
                         required
                       />
                     </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        label="Quê quán"
+                        placeholder="Nhập tên"
+                        fullWidth
+                        required
+                        select
+                        SelectProps={{
+                          native: true,
+                        }}
+                        value={currentProvince.code || 0}
+                        onChange={(e) => {
+                          setCurrentProvince(province[e.target.value]);
+                          getDistrict(+e.target.value);
+                        }}
+                      >
+                        {province.map((option) => (
+                          <option key={option.id} value={option.code}>
+                            {option.name}
+                          </option>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        label="Quận/Huyện"
+                        placeholder="Nhập tên"
+                        fullWidth
+                        required
+                        select
+                        SelectProps={{
+                          native: true,
+                        }}
+                        value={currentDistrict}
+                        onChange={(e) => {
+                          console.log(e.target.value);
+                          setCurrentDistrict(e.target.value);
+                        }}
+                      >
+                        {district?.length > 0 &&
+                          district.map((option) => {
+                            return (
+                              <option key={option.id} value={option.name}>
+                                {option.name}
+                              </option>
+                            );
+                          })}
+                      </TextField>
+                    </Grid>
+
                     <Grid item xs={12}>
                       <TextField
                         label="Email"
@@ -106,9 +256,8 @@ export default function Register() {
                   </Grid>
                 </Grid>
                 {/*right*/}
-                <Grid xs={12} sm={6}>
+                <Grid sm={6}>
                   <Grid container spacing={2}>
-                    
                     <Grid item xs={12}>
                       <TextField
                         label="Số điện thoại"
@@ -143,9 +292,13 @@ export default function Register() {
                         SelectProps={{
                           native: true,
                         }}
-                        value={currentMajor?.id||0}
-                        onChange={(e) =>{
-                          setCurrentMajor(major[e.target.value])
+                        value={currentMajor?.id - 1 || 0}
+                        onChange={(e) => {
+                          setCurrentMajor(major[e.target.value]);
+                          setClassNumber(1);
+                          setClassCount(
+                            major[e.target.value].classCount[0].schoolYear
+                          );
                         }}
                       >
                         {major.map((option) => (
@@ -155,14 +308,115 @@ export default function Register() {
                         ))}
                       </TextField>
                     </Grid>
+                    <Grid xs={12} sm={6} className="mt-2 px-2">
+                      <TextField
+                        label="Niên khóa"
+                        placeholder="Nhập lớp"
+                        fullWidth
+                        required
+                        select
+                        SelectProps={{
+                          native: true,
+                        }}
+                        onChange={(e) => {
+                          setClassCount(e.target.value);
+                          setClassNumber(1);
+                        }}
+                        value={classCount}
+                      >
+                        {currentMajor?.classCount?.map((option, key) => {
+                          return (
+                            <option key={key} value={option.schoolYear}>
+                              {option.schoolYear}
+                            </option>
+                          );
+                        })}
+                      </TextField>
+                    </Grid>
+                    <Grid xs={12} sm={6} className="mt-2 pr-2">
+                      <TextField
+                        label="Lớp"
+                        placeholder="Nhập lớp"
+                        fullWidth
+                        required
+                        select
+                        SelectProps={{
+                          native: true,
+                        }}
+                        value={classNumber}
+                        onChange={(e) => {
+                          setClassNumber(e.target.value);
+                        }}
+                      >
+                        {currentMajor?.classCount?.map((option) => {
+                          if (option.schoolYear !== +classCount) return null;
+
+                          const optionsArray = Array.from(
+                            { length: option.count },
+                            (_, index) => index + 1
+                          );
+
+                          return optionsArray.map((i) => (
+                            <option key={i} value={i}>
+                              {i}
+                            </option>
+                          ));
+                        })}
+                      </TextField>
+                    </Grid>
+                    {/*interest*/}
+                    <Grid item xs={12} fullWidth>
+                      <FormControl sx={{ m: 1, width: 300 }} fullWidth>
+                        <InputLabel id="demo-multiple-chip-label">
+                          Sở thích
+                        </InputLabel>
+                        <Select
+                          labelId="demo-multiple-chip-label"
+                          id="demo-multiple-chip"
+                          multiple
+                          value={personName}
+                          onChange={handleChange}
+                          input={
+                            <OutlinedInput
+                              id="select-multiple-chip"
+                              label="Chip"
+                            />
+                          }
+                          renderValue={(selected) => (
+                            <Box
+                              sx={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                                gap: 0.5,
+                              }}
+                            >
+                              {selected.map((value) => (
+                                <Chip key={value} label={value} />
+                              ))}
+                            </Box>
+                          )}
+                          MenuProps={MenuProps}
+                        >
+                          {names.map((name) => (
+                            <MenuItem
+                              key={name}
+                              value={name}
+                              style={getStyles(name, personName, theme)}
+                            >
+                              {name}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </Grid>
                   </Grid>
                 </Grid>
-                <Grid align="center" className="mt-4">
-                  {" "}
-                  <Button type="submit" color="primary" variant="contained">
-                    Đăng ký
-                  </Button>
-                </Grid>
+              </Grid>
+              <Grid align="center" className="mt-4">
+                {" "}
+                <Button type="submit" color="primary" variant="contained">
+                  Đăng ký
+                </Button>
               </Grid>
             </form>
           </Paper>
