2a496df65d64620672c0a822c2269f2d7bdec304
yamadaOliva <vietanh87.forwork@gmail.com>
2024-06-07 12:02:12 +0700

    huhuh

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://192.168.1.14:8001";
+const URL = "http://localhost:8001";
 
 export const socket = io(URL);
